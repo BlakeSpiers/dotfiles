b2840c99275b26370977620d63fe472160f92506
Blake <blakespiers93@gmail.com>
2023-10-22 16:58:50 +1100

    nvim: add telescope-fzf-native

--- nvim/README.md
+++ nvim/README.md
@@ -1,1 +1,12 @@
-Prerequisite: install [ripgrep](https://github.com/BurntSushi/ripgrep).
+# Neovim
+
+## Prerequisites 
+install [ripgrep](https://github.com/BurntSushi/ripgrep).
+install cmake
+
+## Todo
+Lsp config
+Treesitter
+Snippets
+cmp (completion engine)
+TypescriptOrganiseImports
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,6 +1,15 @@
 return {
   'nvim-telescope/telescope.nvim', branch = '0.1.x',
-  dependencies = { 'nvim-lua/plenary.nvim' },
+  dependencies = { 
+      { 'nvim-lua/plenary.nvim' },
+      { 'nvim-telescope/telescope-fzf-native.nvim', 
+        --build = 'make' 
+        build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build',
+        config = function()
+          require("telescope").load_extension("fzf")
+        end,
+      },
+  },
   keys = {
     {
       "<leader>ff",
@@ -10,7 +19,7 @@ return {
       desc = "Find File"
     },
     {
-      "<leader>fg",
+      "<leader>fl",
       "<cmd>Telescope live_grep<cr>",
       desc = "Find Grep"
     },
@@ -27,6 +36,13 @@ return {
         require("telescope.builtin").help_tags()
       end,
       desc = "Find Help Tags"
+    },
+    {
+      "<leader>fg",
+      function()
+        require("telescope.builtin").grep_string({ shorten_path = true, word_match = "-w", only_sort_text = true, search = ''})
+      end,
+      desc = "Find Help Tags"
     }
-  }
+  },
 }
