62b016ad846d1366cf72a45f610f190752b89293
Blake <blakespiers93@gmail.com>
2023-10-29 21:41:19 +1100

    nvim: add linting

--- nvim/lua/plugins/lsp/mason.lua
+++ nvim/lua/plugins/lsp/mason.lua
@@ -1,59 +1,57 @@
 return {
-    "williamboman/mason.nvim",
-    dependencies = {
-        "williamboman/mason-lspconfig.nvim",
-        "whoIsSethDaniel/mason-tool-installer.nvim"
-    },
-    config = function()
-        local mason = require("mason")
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"whoIsSethDaniel/mason-tool-installer.nvim",
+	},
+	config = function()
+		local mason = require("mason")
 
-        local mason_lspconfig = require("mason-lspconfig")
+		local mason_lspconfig = require("mason-lspconfig")
 
-        local mason_tool_installer = require'mason-tool-installer'
+		local mason_tool_installer = require("mason-tool-installer")
 
-        -- enable mason and configure icons
-        mason.setup({
-            ui = {
-                -- for icons:
-                -- <C-v> uUNICODE
-                -- <C-v> UUNICODE for longer strings
-                -- <C-v>uf42e : 
-                -- <C-v>Uf0792<C-c> : 󰞒
-                icons = {
-                    package_installed = , --f42e
-                    package_pending = 󰞒, --f0792
-                    package_uninstalled = , --f467
-                }
-            }
-        })
+		-- enable mason and configure icons
+		mason.setup({
+			ui = {
+				-- for icons:
+				-- <C-v> uUNICODE
+				-- <C-v> UUNICODE for longer strings
+				-- <C-v>uf42e : 
+				-- <C-v>Uf0792<C-c> : 󰞒
+				icons = {
+					--	 package_installed = , --f42e
+					--	  package_pending = 󰞒, --f0792
+					--	  package_uninstalled = , --f467
+				},
+			},
+		})
 
-        mason_lspconfig.setup({
-            -- list of language servers for mason to install
-            ensure_installed = {
-                "tsserver",
-                "html",
-                "cssls",
-                "tailwindcss",
-                "lua_ls",
-                "pyright",
-                "svelte",
-            },
+		mason_lspconfig.setup({
+			-- list of language servers for mason to install
+			ensure_installed = {
+				"tsserver",
+				"html",
+				"cssls",
+				"tailwindcss",
+				"lua_ls",
+				"pyright",
+				"svelte",
+			},
 
-            -- auto-install configured servers (with lspconfig)
-            automatic_installation = true, -- not the same as ensure_installed
-        })
+			-- auto-install configured servers (with lspconfig)
+			automatic_installation = true, -- not the same as ensure_installed
+		})
 
-        mason_tool_installer.setup({
-            ensure_installed = {
-                "prettier",
-                "stylua",
-                "eslint_d",
-                ---- python
-                -- "isort",
-                -- "black",
-                -- "pylint",
-            }
-        })
-
-    end,
+		mason_tool_installer.setup({
+			ensure_installed = {
+				"prettier",
+				"stylua", --lua formatter
+				"eslint_d",
+				-- "isort", --python formatter
+				-- "black", -- python formatter
+				-- "pylint", -- python linter
+			},
+		})
+	end,
 }
