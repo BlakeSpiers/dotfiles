198aceb68575918bf06377f8f3d313db8da569f9
Blake <blakespiers93@gmail.com>
2023-10-29 21:21:31 +1100

    nvim: add hybrid line numbers and vertical movement keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,1 +1,7 @@
 vim.g.mapleader = " "
+
+-- Panning up and down half a page also centres cursor
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
