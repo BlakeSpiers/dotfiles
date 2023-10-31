899739274be37da2e0e198dd950273ce534e716a
Blake Spiers <b.spiers@nib.com.au>
2023-10-31 15:14:25 +1100

    nvim: Change telescope layout to vertical

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,71 +1,74 @@
 return {
-    'nvim-telescope/telescope.nvim', branch = '0.1.x',
-    dependencies = { 
-        'nvim-lua/plenary.nvim',
-        { 'nvim-telescope/telescope-fzf-native.nvim', 
-            build = 'make' ,
-            --build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build',
-        },
-        'nvim-tree/nvim-web-devicons',
-    },
-    config = function()
-        local telescope = require("telescope")
-        local actions = require("telescope.actions")
+	"nvim-telescope/telescope.nvim",
+	branch = "0.1.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{
+			"nvim-telescope/telescope-fzf-native.nvim",
+			build = "make",
+			--build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build',
+		},
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		local telescope = require("telescope")
+		local actions = require("telescope.actions")
 
-        telescope.setup({
-            defaults = {
-                mappings = {
-                    i = {
-                        ["<C-k>"] = actions.move_selection_previous,
-                        ["<C-j>"] = actions.move_selection_next,
-                        ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-                    }
-                }
-            }
-        })
+		telescope.setup({
+			defaults = {
+				mappings = {
+					i = {
+						["<C-k>"] = actions.move_selection_previous,
+						["<C-j>"] = actions.move_selection_next,
+						["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+					},
+				},
+				layout_strategy = "vertical",
+			},
+		})
 
-        telescope.load_extension("fzf");
-    end,
-    keys = {
-        {
-            "<leader>ff",
-            function()
-                require("telescope.builtin").find_files()
-            end,
-            desc = "Find file (fuzzy)"
-        },
-        {
-            "<leader>fr",
-            function()
-                require("telescope.builtin").oldfiles()
-            end,
-            desc = "Find recent files (fuzzy)"
-        },
-        {
-            "<leader>fs",
-            "<cmd>Telescope live_grep<cr>",
-            desc = "Find string"
-        },
-        {
-            "<leader>fc",
-            function()
-                require("telescope.builtin").grep_string()
-            end,
-            desc = "Find string under cursor"
-        },
-        {
-            "<leader>fb",
-            function()
-                require("telescope.builtin").buffers()
-            end,
-            desc = "Find buffers"
-        },
-        {
-            "<leader>fh",
-            function()
-                require("telescope.builtin").help_tags()
-            end,
-            desc = "Find help tags"
-        },
-    },
+		telescope.load_extension("fzf")
+	end,
+	keys = {
+		{
+			"<leader>ff",
+			function()
+				require("telescope.builtin").find_files()
+			end,
+			desc = "Find file (fuzzy)",
+		},
+		{
+			"<leader>fr",
+			function()
+				require("telescope.builtin").oldfiles()
+			end,
+			desc = "Find recent files (fuzzy)",
+		},
+		{
+			"<leader>fs",
+			"<cmd>Telescope live_grep<cr>",
+			desc = "Find string",
+		},
+		{
+			"<leader>fc",
+			function()
+				require("telescope.builtin").grep_string()
+			end,
+			desc = "Find string under cursor",
+		},
+		{
+			"<leader>fb",
+			function()
+				require("telescope.builtin").buffers()
+			end,
+			desc = "Find buffers",
+		},
+		{
+			"<leader>fh",
+			function()
+				require("telescope.builtin").help_tags()
+			end,
+			desc = "Find help tags",
+		},
+	},
 }
