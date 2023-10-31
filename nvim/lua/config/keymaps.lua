16b3182c1c38612c58d79f85dcc861e3d5df413f
Blake Spiers <b.spiers@nib.com.au>
2023-10-31 14:32:18 +1100

    nvim: add nvim-surround

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -3,5 +3,6 @@ vim.g.mapleader = " "
 -- Panning up and down half a page also centres cursor
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+-- next and next (backward search) also centres the cursor
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
